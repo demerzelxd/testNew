f3d1bb6ddefb49f7f8097689279e0531c51b698f
demerzelxd <demerzelxd@gmail.com>
2022-08-08 19:35:12 +0800

    引入jasypt

--- src/test/java/cn/me/myboot/MyBootApplicationTests.java
+++ src/test/java/cn/me/myboot/MyBootApplicationTests.java
@@ -1,13 +1,23 @@
 package cn.me.myboot;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyBootApplicationTests {
 
+    @Autowired
+    private StringEncryptor encryptor;
+
     @Test
     void contextLoads() {
+        String password = "12345";
+        String encryptPwd = encryptor.encrypt(password);
+        System.out.println("加密: " + encryptPwd);
+        System.out.println("解密: " + encryptor.decrypt(encryptPwd));
     }
 
 }
