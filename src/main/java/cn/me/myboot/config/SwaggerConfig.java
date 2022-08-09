c8e4102b84d6ed42e8ec393d24d8ea593ce617af
demerzelxd <demerzelxd@gmail.com>
2022-08-09 17:07:08 +0800

    引入swagger

---
+++ src/main/java/cn/me/myboot/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package cn.me.myboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author 李心达
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.me.myboot.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger")
+                        .description("SpringBoot整合Swagger，详细信息......")
+                        .version("9.0")
+                        .contact(new Contact("Giskard", "getschwifty.cn", "test.com"))
+                        .license("The Giskard License")
+                        .licenseUrl("http://www.getschwifty.cn")
+                        .build());
+    }
+}
--- src/main/java/cn/me/myboot/controller/TbUserController.java
+++ src/main/java/cn/me/myboot/controller/TbUserController.java
@@ -5,11 +5,14 @@ import cn.me.myboot.model.dto.UserDetailDTO;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/user")
+@Api(tags = "用户Controller")
 public class TbUserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TbUserController.class);
@@ -28,9 +32,12 @@ public class TbUserController {
     private TbUserMapper userMapper;
 
     @PostMapping("/select")
-    public String selectUserInfo() {
+    @ApiOperation(value = "分页查询用户信息接口",
+            notes = "<span style='color:red;'>描述:</span>用来分页查询用户信息的接口")
+    public String selectUserInfo(@RequestParam(value = "pageIndex", defaultValue = "1", required = false) Integer pageIndex,
+                                 @RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize) {
         // 分页参数
-        IPage<UserDetailDTO> page = new Page<>(1, 10);
+        IPage<UserDetailDTO> page = new Page<>(pageIndex, pageSize);
         // 多表queryWrapper分页查询，若为单表，直接用selectPage即可
         IPage<UserDetailDTO> pageResult = this.userMapper.listUserDetail(page, new QueryWrapper<UserDetailDTO>()
                 .like("a.username", "dada").orderByAsc("a.id"));
